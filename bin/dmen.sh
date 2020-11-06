5fd993649641e05d1112082c5ba96e2cbae31429
Ajibola Okubanjo <okubax@gmail.com>
2020-11-06 11:22:54 +0000

    font changes & new scripts

--- bin/dmen.sh
+++ bin/dmen.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 # Import the colors
-. "${HOME}/.Xresources.d/colors/nord/nord.sh"
+	. "${HOME}/.Xresources.d/colors/nord/nord.sh"
 
-j4-dmenu-desktop --term urxvt --display-binar --dmenu "dmenu -b  -sb '$color8' -sf '$color0' -nb '$color0' -nf '$color4' -fn 'mononoki-7'"
+j4-dmenu-desktop --term urxvt --display-binar --dmenu "dmenu -b  -sb '$color8' -sf '$color0' -nb '$color0' -nf '$color4' -fn 'cascadia code-7'"
