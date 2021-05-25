952a337f1001d5e66bc0e921ead69d73914d17e4
Ajibola Okubanjo <okubax@gmail.com>
2021-05-25 20:01:20 +0100

    cleanup & updated scripts for new system

--- bin/clipmenu.sh
+++ bin/clipmenu.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-clipmenu -b -i -sb '#88C0D0' -sf '#2E3440' -nb '#2E3440' -nf '#D8DEE9' -fn 'cascadia code-7'
+clipmenu -b -i -sb '#88C0D0' -sf '#2E3440' -nb '#2E3440' -nf '#D8DEE9' -fn 'cascadia code-9'
--- bin/dmen.sh
+++ bin/dmen.sh
@@ -3,4 +3,4 @@
 # Import the colors
 	. "${HOME}/.Xresources.d/colors/nord/nord.sh"
 
-j4-dmenu-desktop --term urxvt --display-binar --dmenu "dmenu -b  -sb '$color8' -sf '$color0' -nb '$color0' -nf '$color4' -fn 'cascadia code-7'"
+j4-dmenu-desktop --term urxvt --display-binar --dmenu "dmenu -b  -sb '$color8' -sf '$color0' -nb '$color0' -nf '$color4' -fn 'cascadia code-9'"
